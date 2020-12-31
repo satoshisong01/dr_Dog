c5cd85b36a9b1c1e3bc8ea108ba223e6d7b1d6cc
songkyoungsuk <rudtjrch@naver.com>
2020-12-31 21:26:26 +0900

    아이탬 line clamp ,Js 버튼 토글

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
+const moreBtn = document.querySelectorAll('.textBtn');
+const texttitle = document.querySelectorAll('.item__text');
 
 toggleBtn.addEventListener('click', () =>{
     menu.classList.toggle('active');
 });
+/*
+moreBtn.addEventListener('click', () =>{
+    moreBtn.classList.toggle('clicked');
+    texttitle.classList.toggle('clamp');
+})
+*/
+for( let i = 0; i < moreBtn.length; i++){
+    moreBtn[i].addEventListener('click', ()=>{
+        moreBtn[i].classList.toggle('clicked');
+        texttitle[i].classList.toggle('clamp');
+    })
+}
+
